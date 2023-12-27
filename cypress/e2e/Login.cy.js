29784e8010b8d4ad016bea623dc9aa97f12164cc
Sami Ullah <sami.ullah@axelliant.com>
2023-12-27 00:41:24 -0800

    Unsuccesful login test cases

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -19,5 +19,13 @@ describe('Login Test Suite', () => {
     
   })
  
+  it('unsuccessful login with valid email and invalid password', () => {
+    cy.visit('https://rover-frontend-dev.azurewebsites.net/auth/login')
+    
+    cy.successfull_Login();
+    
+    cy.Logout();
+    
+  })
 
 })
